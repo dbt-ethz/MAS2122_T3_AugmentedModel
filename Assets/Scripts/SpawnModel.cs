abe4e34f1b0ef4073dec6e39308cca6a2981d510
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2022-07-06 13:17:03 +0200

    update

---
+++ Assets/Scripts/SpawnModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnModel : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log("start to spawn tower model");
+    }
+}
